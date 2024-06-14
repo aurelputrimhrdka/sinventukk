a68414ce23b328e3c926d8c98e7e5dde8341bae5
root <root@aurel.top>
2024-06-14 14:04:21 +0700

    fix

--- resources/views/barang/create.blade.php
+++ resources/views/barang/create.blade.php
@@ -24,7 +24,6 @@
                                 @enderror
                             </div>
 
-
                             <div class="form-group">
                                 <label class="font-weight-bold">SERI</label>
                                 <input type="text" class="form-control @error('seri') is-invalid @enderror" name="seri" value="{{ old('seri') }}" placeholder="Masukkan Seri Barang">
@@ -36,7 +35,6 @@
                                     </div>
                                 @enderror
                             </div>
-
 
                             <div class="form-group">
                                 <label class="font-weight-bold">SPESIFIKASI</label>
@@ -50,10 +48,9 @@
                                 @enderror
                             </div>
 
-
                             <div class="form-group">
                                 <label class="font-weight-bold">STOK</label>
-                                <input type="text" class="form-control @error('stok') is-invalid @enderror" name="stok" value="{{ old('stok') }}" placeholder="Masukkan Stok Barang pada form barang masuk!" disabled>
+                                <input type="number" min="0" class="form-control @error('stok') is-invalid @enderror" name="stok" value="0" placeholder="0" readonly>
                            
                                 <!-- error message untuk stok -->
                                 @error('stok')
@@ -66,9 +63,9 @@
                             <div class="form-group">
                                 <label class="font-weight-bold">KATEGORI</label>
                                 <select class="form-control" name="kategori_id" aria-label="Default select example">
-                                    <option value="blank">Pilih Deskripsi</option>
+                                    <option value="blank">Pilih Kategori</option>
                                     @foreach ($akategori as $rowkategori)
-                                        <option value="{{ $rowkategori->id  }}">{{ $rowkategori->deskripsi }}</option>
+                                        <option value="{{ $rowkategori->id  }}">{{ $rowkategori->deskripsi  }}</option>
                                     @endforeach
                                 </select>
                                
@@ -79,22 +76,9 @@
                                     </div>
                                 @enderror
                             </div>
-                            
-                            <!-- <div class="form-group">
-                                <label class="font-weight-bold">FOTO</label>
-                                <input type="file" class="form-control @error('foto') is-invalid @enderror" name="foto" value="{{ old('foto') }}" placeholder="Masukkan Foto Barang"> -->
-                           
-                                <!-- error message untuk foto -->
-                                <!-- @error('foto')
-                                    <div class="alert alert-danger mt-2">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
-                            </div> -->
 
                             <button type="submit" class="btn btn-md btn-primary">SIMPAN</button>
                             <button type="reset" class="btn btn-md btn-warning">RESET</button>
-                            <a href="{{ route('barang.index') }}" class="btn btn-md btn-primary">Back</a>
 
                         </form>
                     </div>
--- resources/views/barang/edit.blade.php
+++ resources/views/barang/edit.blade.php
@@ -1,20 +1,22 @@
 @extends('layouts.adm-main')
 
+
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-		<div class="pull-left">
-		    <h2>EDIT BARANG</h2>
-		</div>
+                <div class="pull-left">
+                    <h2>EDIT BARANG</h2>
+                </div>
                 <div class="card">
                     <div class="card-body">
                         <form action="{{ route('barang.update',$rsetBarang->id) }}" method="POST" enctype="multipart/form-data">                    
                             @csrf
                             @method('PUT')
 
+
                             <div class="form-group">
-                                <label class="font-weight-bold">MERK BARANG</label>
+                                <label class="font-weight-bold">NAMA</label>
                                 <input type="text" class="form-control @error('merk') is-invalid @enderror" name="merk" value="{{ old('merk',$rsetBarang->merk) }}" placeholder="Masukkan Merk Barang">
                            
                                 <!-- error message untuk merk -->
@@ -25,8 +27,9 @@
                                 @enderror
                             </div>
 
+
                             <div class="form-group">
-                                <label class="font-weight-bold">SERI BARANG</label>
+                                <label class="font-weight-bold">SERI</label>
                                 <input type="text" class="form-control @error('seri') is-invalid @enderror" name="seri" value="{{ old('seri',$rsetBarang->seri) }}" placeholder="Masukkan Seri Barang">
                            
                                 <!-- error message untuk seri -->
@@ -36,9 +39,10 @@
                                     </div>
                                 @enderror
                             </div>
-                            
+
+
                             <div class="form-group">
-                                <label class="font-weight-bold">SPESIFIKASI BARANG</label>
+                                <label class="font-weight-bold">SPESIFIKASI</label>
                                 <input type="text" class="form-control @error('spesifikasi') is-invalid @enderror" name="spesifikasi" value="{{ old('spesifikasi',$rsetBarang->spesifikasi) }}" placeholder="Masukkan Spesifikasi Barang">
                            
                                 <!-- error message untuk spesifikasi -->
@@ -48,11 +52,11 @@
                                     </div>
                                 @enderror
                             </div>
-                            
+
+
                             <div class="form-group">
-                                <label class="font-weight-bold">STOK BARANG</label>
-                                <input type="text" class="form-control @error('stok') is-invalid @enderror" name="stok" value="{{ old('stok',$rsetBarang->stok) }}" placeholder="Masukkan Stok Barang" disabled>
-                           
+                                <label class="font-weight-bold">STOK</label>
+                                <input type="number" class="form-control @error('stok') is-invalid @enderror" name="stok" value="{{ old('stok',$rsetBarang->stok) }}" placeholder="Masukkan Stok Barang" readonly>
                                 <!-- error message untuk stok -->
                                 @error('stok')
                                     <div class="alert alert-danger mt-2">
@@ -61,19 +65,19 @@
                                 @enderror
                             </div>
 
+
                             <div class="form-group">
                                 <label class="font-weight-bold">KATEGORI</label>
                                 <select class="form-control" name="kategori_id" aria-label="Default select example">
-                                    <option value="blank">Pilih Deskripsi</option>
-                                    @foreach ($akategori as $rowkategori)
-                                        @if ($selectedKategori && $selectedKategori->id == $rowkategori->id)
-                                            <option value="{{ $rowkategori->id }}" selected>{{ $rowkategori->deskripsi }}</option>
+                                    @foreach ($akategori as $kategori)
+                                        @if ($selectedKategori && $selectedKategori->id == $kategori->id)
+                                            <option value="{{ $kategori->id }}" selected>{{ $kategori->deskripsi }}</option>
                                         @else
-                                            <option value="{{ $rowkategori->id }}">{{ $rowkategori->deskripsi }}</option>
+                                            <option value="{{ $kategori->id }}">{{ $kategori->deskripsi }}</option>
                                         @endif
                                     @endforeach
                                 </select>
-                               
+                            
                                 <!-- error message untuk kategori -->
                                 @error('kategori_id')
                                     <div class="alert alert-danger mt-2">
@@ -82,21 +86,10 @@
                                 @enderror
                             </div>
 
-                            <!-- <div class="form-group">
-                                <label class="font-weight-bold">FOTO</label>
-                                <input type="file" class="form-control @error('foto') is-invalid @enderror" name="foto"> -->
-                           
-                                <!-- error message untuk foto -->
-                                <!-- @error('foto')
-                                    <div class="alert alert-danger mt-2">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
-                            </div> -->
 
                             <button type="submit" class="btn btn-md btn-primary">SIMPAN</button>
                             <button type="reset" class="btn btn-md btn-warning">RESET</button>
-                            <a href="{{ route('barang.index') }}" class="btn btn-md btn-primary">BACK</a>
+
 
                         </form>
                     </div>
