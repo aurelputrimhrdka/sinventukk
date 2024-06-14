31bcf3aea33430f21518198de861c1a7e4e82c18
root <root@aurel.top>
2024-06-14 14:25:32 +0700

    [invent-01] Lihat Semua kategori

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\KategoriController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-use App\Http\Controllers\Api\KategoriController;
 Route::apiResource('kategori', KategoriController::class);
